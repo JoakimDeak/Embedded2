534e2254debd7c50a8e82e286c0a610f38092592
unknown <joakimdeak@hotmail.com>
2020-02-12 22:50:05 +0100

    finilized 3.4

--- exerc_3_4.c
+++ exerc_3_4.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <unistd.h>
-#include <stdlib.h>
 #include <string.h>
 
 typedef struct{
@@ -9,24 +8,30 @@ typedef struct{
     char pers_number[13]; // yyyymmddxxxx
 }Person;
 
-Person inputRecord(){
-
+void appendFile(Person *inRecord){
+    FILE *fout;
+    if((fout = fopen("personDB.bin", "ab")) == NULL){
+        printf("Couldn't open file to write\n");
+        return;
+    }
+    if((fwrite(inRecord, sizeof(Person), 1, fout)) == -1){
+        printf("Couldn't write to file write\n");
+        return;
+    }
+    fclose(fout);
 }
 
 void writeNewFile(Person *inRecord){
+    printf("What gets passed :%s, %s, %s\n", inRecord->firstName, inRecord->famName, inRecord->pers_number);
     FILE *ptr;
     if (access ("personDB.bin", F_OK) != -1){
         //FILE EXISTS
         remove("personDB.bin");
-        printf("Successfully deleted /n");
+        printf("Successfully deleted \n");
     }
 
-    ptr = fopen("personDB.bin", "wb");
-    if((fwrite(inRecord, sizeof(Person), 1, ptr)) == -1){
-        printf("Couldn't write to file write\n");
-    } else{
-        printf("Successfully wrote to the write\n");
-    }
+    appendFile(inRecord);
+
     printf("Made file");
 
 
@@ -39,15 +44,7 @@ void printFile(){
         printf("Couldn't open file to read\n");
         return;
     }
-    /*while(!feof(fin)){ // repeat until end of file is reached
-        if((fread(&pr, sizeof(Person), 1, fin)) == -1){
-            printf("Couldn't read from file\n");
-            fclose(fin);
-            return;
-        } else{
-            printf("First Name: %s\n Last Name: %s\n Personnummer: %s\n", pr.firstName, pr.famName, pr.pers_number);
-        }
-    }*/
+
     while(1){
         fread(&pr, sizeof(Person), 1, fin);
         if(feof(fin) != 0){
@@ -68,34 +65,20 @@ void searchByFirstName(char *name){
         return;
     }
     while(1){
-        printf("we are in da loop");
         fread(&pr, sizeof(Person), 1, fin);
         if(feof(fin) != 0){
             break;
         } else{
             if(strcmp(pr.firstName, name) == 0 || strcmp(pr.famName, name) == 0) {
-                printf("SAME");
-                printf("Searched for Kent: Given record:  "
-                       "First Name: %s\n Last Name: %s\n Personnummer: %s\n", pr.firstName, pr.famName, pr.pers_number);
+                printf("Searched for %s:\nGiven record:\n"
+                       "First Name: %s\nLast Name: %s\nPersonnummer: %s\n", name, pr.firstName, pr.famName, pr.pers_number);
+                break;
             }
         }
     }
 
     fclose(fin);
 
-}
-
-void appendFile(Person *inRecord){
-    FILE *fout;
-    if((fout = fopen("personDB.bin", "ab")) == NULL){
-        printf("Couldn't open file to write\n");
-        return;
-    }
-    if((fwrite(inRecord, sizeof(Person), 1, fout)) == -1){
-        printf("Couldn't write to file write\n");
-        return;
-    }
-    fclose(fout);
 }
 
 int main(){
@@ -166,27 +149,6 @@ int main(){
             }
         }
     } while(choice != 5);
-/*
-    char firstName[20];
-    char famName[20];
-    char pers_number[13];
-
-    printf("Enter first name\n");
-    fgets(firstName, 20, stdin);
-
-    printf("Enter last name\n");
-    fgets(famName, 20, stdin);
-
-    printf("Enter personnummer\n");
-    fgets(pers_number, 13, stdin);
-
-    Person pr;
-    strcpy(pr.firstName, firstName);
-    strcpy(pr.famName, famName);
-    strcpy(pr.pers_number, pers_number);
-
-    appendFile(&pr);
-    printFile();*/
 
     return 0;
 }
