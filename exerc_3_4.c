05cc63079adcd43a26223bf764ccb3af498f36ca
joakim deak <joakimdeak@hotmail.com>
2020-02-12 20:00:33 +0100

    started implementing appendFile

--- exerc_3_4.c
+++ exerc_3_4.c
@@ -4,21 +4,15 @@ typedef struct{
     char firstName[20];
     char famName[20];
     char pers_number[13]; // yyyymmddxxxx
+    int test;
 }Person;
-
-int main(){
-
-
-
-    return 0;
-}
 
 Person inputRecord(){
 
 }
 
 void writeNewFile(Person *inRecord){
-    
+
 }
 
 void printFile(){
@@ -30,5 +24,29 @@ void searchByFirstName(char *name){
 }
 
 void appendFile(Person *inRecord){
+    FILE *fout;
+    if((fout = fopen("personDB.bin", "ab")) == NULL){
+        printf("Couldnt open file\n");
+        return;
+    } else{
+        printf("could open file\n");
+    }
+    fwrite(&inRecord, sizeof(Person), 1, fout);
+    fclose(fout);
 
+
+
+    Person* pr;
+    FILE *fin = fopen("personDB.bin", "rb");
+    fread(&pr, sizeof(Person), 1, fin);
+    printf("%s, %s, %s, %d", pr->firstName, pr->famName, pr->pers_number, pr->test);
+    fclose(fin);
+}
+
+int main(){
+
+    Person p = {"joakim", "Deak", "199910064196", 6};
+    appendFile(&p);
+
+    return 0;
 }
