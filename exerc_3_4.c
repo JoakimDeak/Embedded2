ad444442d5ec4ddb5985fa7853c6322a6f529d45
unknown <joakimdeak@hotmail.com>
2020-02-12 20:34:16 +0100

    implemented appendFile

--- exerc_3_4.c
+++ exerc_3_4.c
@@ -4,7 +4,6 @@ typedef struct{
     char firstName[20];
     char famName[20];
     char pers_number[13]; // yyyymmddxxxx
-    int test;
 }Person;
 
 Person inputRecord(){
@@ -26,27 +25,20 @@ void searchByFirstName(char *name){
 void appendFile(Person *inRecord){
     FILE *fout;
     if((fout = fopen("personDB.bin", "ab")) == NULL){
-        printf("Couldnt open file\n");
+        printf("Couldn't open file to write\n");
         return;
     } else{
-        printf("could open file\n");
+        printf("could open file to write\n");
     }
-    fwrite(&inRecord, sizeof(Person), 1, fout);
+    if((fwrite(inRecord, sizeof(Person), 1, fout)) == -1){
+        printf("Couldn't write to file write\n");
+    } else{
+        printf("Successfully wrote to the write\n");
+    }
     fclose(fout);
-
-
-
-    Person* pr;
-    FILE *fin = fopen("personDB.bin", "rb");
-    fread(&pr, sizeof(Person), 1, fin);
-    printf("%s, %s, %s, %d", pr->firstName, pr->famName, pr->pers_number, pr->test);
-    fclose(fin);
 }
 
 int main(){
-
-    Person p = {"joakim", "Deak", "199910064196", 6};
-    appendFile(&p);
 
     return 0;
 }
