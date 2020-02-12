5394270774a9d6673bc8d3d57a9b7b280b040fe0
joakim deak <joakimdeak@hotmail.com>
2020-02-12 16:51:49 +0100

    added method signatures

---
+++ exerc_3_4.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+typedef struct{
+    char firstName[20];
+    char famName[20];
+    char pers_number[13]; // yyyymmddxxxx
+}Person;
+
+int main(){
+
+
+
+    return 0;
+}
+
+Person inputRecord(){
+
+}
+
+void writeNewFile(Person *inRecord){
+    
+}
+
+void printFile(){
+
+}
+
+void searchByFirstName(char *name){
+
+}
+
+void appendFile(Person *inRecord){
+
+}
