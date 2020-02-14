2bf29c2e5ebce936ff1340431e3711ef2f783520
unknown <joakimdeak@hotmail.com>
2020-02-14 22:38:04 +0100

    added header to 4.2

--- decode.c
+++ decode.c
@@ -1,3 +1,13 @@
+/* ====================================
+File name: exerc_2_3.h
+Date: 2020-02-06
+Group nr 19
+Members that contribute to the solutions
+Joakim Deak
+Member not present at the demonstration time
+
+Demonstration code:
+====================================== */
 #include <stdio.h>
 #include <stdlib.h>
 
