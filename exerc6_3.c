1bd10028f66514cfd00449d06719da9d0e88a830
joakim deak <joakimdeak@hotmail.com>
2020-03-10 13:00:46 +0100

    fixed reg8

--- exerc6_3.c
+++ exerc6_3.c
@@ -10,11 +10,13 @@ Demonstration code:
 ====================================== */
 #include <stdio.h>
 
-//#define ML13_Status 0xB10
-//define ML13_Control 0x0B00
+#define ML13_Status 0xB10
+#define ML13_Control 0x0B00
 
-unsigned char ML13_Status = 0xB10;
-unsigned char ML13_Control = 0x0B00;
+#define REG8(x) *((unsigned char *)(x))
+
+unsigned char Status = REG8(ML13_Status);
+unsigned char Control = REG8(ML13_Control);
 
 void delay(x){
     for(int i = 0; i < x; i++);
@@ -22,29 +24,29 @@ void delay(x){
 
 void door(){
     while(1){ // run forever unless door breaks down
-        if(ML13_Status >> 3 & 1){ // if sensor activated
-            ML13_Control |= 1 << 0; // open the door
-            ML13_Status |= 1 << 6; // opening slowly
-            ML13_Status &= ~(1 << 5); // door is not closed
+        if(Status >> 3 & 1){ // if sensor activated
+            Control |= 1 << 0; // open the door
+            Status |= 1 << 6; // opening slowly
+            Status &= ~(1 << 5); // door is not closed
             delay(10);
-            ML13_Status |= 1 << 4; // door is wide open
-            ML13_Status &= ~(1 << 6); // door is not opening
+            Status |= 1 << 4; // door is wide open
+            Status &= ~(1 << 6); // door is not opening
         }
 
         for(int i = 0; i < 100; i++){ // will stay for as long as the sensor gets activated before i reaches 100
             delay(10);
-            if(ML13_Status >> 3 & 1){
+            if(Status >> 3 & 1){
                 i = 0;
             }
         }
 
-        ML13_Control |= 1 << 1; // close the door
-        ML13_Status |= 1 << 7; // closing door
+        Control |= 1 << 1; // close the door
+        Status |= 1 << 7; // closing door
         delay(10);
-        ML13_Status &= ~(1 << 4); // door is not open
+        Status &= ~(1 << 4); // door is not open
 
-        ML13_Status |= 1 << 5; // door is closed
-        ML13_Status &= ~(1 << 7); // door is not closing
+        Status |= 1 << 5; // door is closed
+        Status &= ~(1 << 7); // door is not closing
 
     }
 }
